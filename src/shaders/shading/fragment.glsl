b0782e632ef5f98d318a6224e0b85d077d088d5f
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2024-09-27 12:23:05 +0300

    fix the darkness

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,26 +1,33 @@
 uniform vec3 uColor;
 varying vec3 vNormal;
+varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 
-vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition)
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection)
 {
-     vec3 lightDirection = normalize(lightPosition);
+    vec3 lightDirection = normalize(lightPosition);
+
     // Shading
     float shading = dot(normal, lightDirection);
+    shading = max(0.0, shading);
+
     return lightColor * lightIntensity *shading;
    
 }
+
 void main()
 {
+    viewDirection = normalize(vPosition - cameraPosition);
+
     vec3 color = uColor;
 
-    //light
+    //Light
     vec3 light = vec3(0.0);
-    // light += ambientLight(
-    // vec3(1.0), // Light color
-    // 0.03       // Light intensity
-    // );
+    light += ambientLight(
+    vec3(1.0), // Light color
+    0.03       // Light intensity
+    );
      light += directionalLight(
     vec3(0.1,0.1,1.0),   // Light color
     1.0,                 // Light intensity
